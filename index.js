be15729ba271f7b0f4b77f049a902687c2615d08
chenxiaochun <sjzchenxiaochun@126.com>
2017-08-15 16:54:45 +0800

    publish webpack-upload-plugin@1.10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var client = require('scp2')
 var fs = require('fs')
+var glob = require('glob')
 
 function webpackUploadPlugin(options){
     this.options = options
@@ -7,15 +8,16 @@ function webpackUploadPlugin(options){
 
 webpackUploadPlugin.prototype.apply = function(compiler){
     var options = this.options
-    var source = process.cwd()
+    var source = `${process.cwd()}/${options.source}`
     var target = options.target
+    var cdn = (options.cdn === 'static' ? 'static.360buyimg.com' : 'misc.360buyimg.com')
     var username = options.username
     var password = options.password
     var host = options.host
     var exclude = options.exclude || `.DS_Store|.babelrc|.git|node_modules|${options.exclude}`
 
     if(!/^\//.test(target)){
-        target = `/var/www/html/page.jd.com/${target}`
+        target = `/var/www/html/${cdn}/${target}`
     }
 
     if(!target){
@@ -31,24 +33,15 @@ webpackUploadPlugin.prototype.apply = function(compiler){
             host: host
         })
 
-        client.mkdir(target, function(error){
-            if(error){
-                console.log(error)
-                client.close()
-            }else{
-                filelist.forEach(function(filepath){
-                    if(!new RegExp(exclude).exec(filepath)){
-                        client.scp(`${source}/${filepath}`, `${username}:${password}@${host}:${target}/${filepath}`, function(error){
-                            if(error){
-                                console.log(error)
-                            }else{
-                                console.log(`Upload ${source}/${filepath} success`)
-                                client.close()
-                            }
-                        });
-                    }
-                })
-            }
+        glob(source, {}, function(error, files){
+            client.scp(files[0], `${username}:${password}@${host}:${target}/${options.source}`, function(error){
+                if(error){
+                    console.log(error)
+                }else{
+                    console.log(`Upload ${source} success`)
+                    client.close()
+                }
+            })
         })
     })
 }
